42f2506b5065a3d2b9c6f6151845b6528737af1d
Глухов Кирилл Викторович <irvinzz@yandex.ru>
2026-03-18 17:19:05 +0500

    confirm dialog implemented

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -2,6 +2,7 @@
 
 set -euo pipefail
 
+node ./scripts/install-mongodb-tools.js
 bash ./scripts/install-playwright-chromium.sh
 node ./scripts/install-mongodb.js
 
--- src/renderer/src/hooks/handle-async-action.tsx
+++ src/renderer/src/hooks/handle-async-action.tsx
@@ -1,6 +1,14 @@
 import { createContext, useCallback, useContext, useState } from 'react';
 
-import { Alert, Backdrop, CircularProgress } from '@mui/material';
+import {
+  Alert,
+  Backdrop,
+  Button,
+  CircularProgress,
+  Dialog,
+  DialogActions,
+  DialogTitle,
+} from '@mui/material';
 import CheckIcon from '@mui/icons-material/Check';
 
 type LoadingContextType = {
@@ -10,6 +18,11 @@ type LoadingContextType = {
   setError: (newValue: string | null) => void;
   progress: number | null;
   setProgress: (newProgress: number | null) => void;
+  confirmDialogVisible: boolean;
+  setConfirmDialogVisible: (visible: boolean) => void;
+  confirmYes: () => void;
+  confirmNo: () => void;
+  setConfirmAnswerHandler: (handler: (answer: 'YES' | 'NO') => void) => void;
 };
 const LoadingContext = createContext<LoadingContextType>({
   loading: false,
@@ -18,12 +31,21 @@ const LoadingContext = createContext<LoadingContextType>({
   setError: () => {},
   progress: null,
   setProgress: () => {},
+  confirmDialogVisible: false,
+  setConfirmDialogVisible: () => {},
+  confirmYes: () => {},
+  confirmNo: () => {},
+  setConfirmAnswerHandler: () => {},
 });
 
 export function LoadingProvider(props: { children?: React.ReactNode }): React.JSX.Element {
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
   const [progress, setProgress] = useState<number | null>(null);
+  const [confirmDialogVisible, setConfirmDialogVisible] = useState(false);
+  const [confirmAnswerHandler, setConfirmAnswerHandler] = useState<
+    ((answer: 'YES' | 'NO') => void) | null
+  >(null);
 
   return (
     <LoadingContext.Provider
@@ -34,6 +56,17 @@ export function LoadingProvider(props: { children?: React.ReactNode }): React.JS
         setError,
         progress,
         setProgress,
+        confirmDialogVisible,
+        setConfirmDialogVisible,
+        confirmYes() {
+          confirmAnswerHandler?.('YES');
+          setConfirmDialogVisible(false);
+        },
+        confirmNo() {
+          confirmAnswerHandler?.('NO');
+          setConfirmDialogVisible(false);
+        },
+        setConfirmAnswerHandler,
       }}
     >
       {props.children}
@@ -44,8 +77,9 @@ export function LoadingProvider(props: { children?: React.ReactNode }): React.JS
 export function useHandleAsyncAction(): {
   handleAsyncAction(cb: () => Promise<void>): void;
   loading: boolean;
+  confirm: () => Promise<'YES' | 'NO'>;
 } {
-  const { loading, setLoading, setError } = useContext(LoadingContext);
+  const { loading, setLoading, setError, setConfirmDialogVisible, setConfirmAnswerHandler } = useContext(LoadingContext);
 
   const handleAsyncAction = useCallback(
     async (cb: () => Promise<void>) => {
@@ -68,29 +102,49 @@ export function useHandleAsyncAction(): {
     [setError, setLoading]
   );
 
+  const confirm = useCallback(async () => {
+    setConfirmDialogVisible(true);
+    return new Promise<'YES' | 'NO'>((resolve) => {
+      setConfirmAnswerHandler(() => (answer) => {
+        resolve(answer);
+      });
+    });
+  }, [setConfirmAnswerHandler, setConfirmDialogVisible]);
+
   return {
     handleAsyncAction,
     loading,
+    confirm,
   };
 }
 
 export function LoadingMask(): React.JSX.Element {
-  const { loading, error, setError } = useContext(LoadingContext);
+  const { loading, error, setError, confirmDialogVisible, confirmYes, confirmNo } = useContext(LoadingContext);
 
   return (
     <>
-      <Backdrop open={loading} sx={(theme) => ({ color: '#fff', zIndex: theme.zIndex.drawer + 1 })}>
+      <Backdrop
+        open={loading}
+        sx={(theme) => ({ color: '#fff', zIndex: theme.zIndex.drawer + 10000 })}
+      >
         <CircularProgress color="inherit" />
       </Backdrop>
       <Backdrop
         open={Boolean(error)}
         onClick={() => setError(null)}
-        sx={(theme) => ({ color: '#fff', zIndex: theme.zIndex.drawer + 1 })}
+        sx={(theme) => ({ color: '#fff', zIndex: theme.zIndex.drawer + 10000 })}
       >
         <Alert icon={<CheckIcon fontSize="inherit" />} severity="error">
           {error}
         </Alert>
       </Backdrop>
+      <Dialog open={confirmDialogVisible} sx={(theme) => ({ zIndex: theme.zIndex.drawer + 10100 })}>
+        <DialogTitle>Are you sure?</DialogTitle>
+        <DialogActions>
+          <Button onClick={() => confirmYes()}>Yes</Button>
+          <Button onClick={() => confirmNo()}>No</Button>
+        </DialogActions>
+      </Dialog>
     </>
   );
 }
